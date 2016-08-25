4e8a1263374a0180a8e088d2b67c8088aaeba1bb
Nicolas <nicolas@Saras-MacBook-Air.local>
2016-08-24 22:24:13 -0400

    Library skeleton

--- dist/panel-gallery-spa.js
+++ dist/panel-gallery-spa.js
@@ -61,7 +61,31 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 1 */
 /***/ function(module, exports) {
 
-	console.log('hi there!');
+	var PanelGallerySPA = (function(){
+
+	    console.log('yo!');
+
+	    let defaults = {
+	        a: 1
+	    };
+	 
+	    var init = function(){
+	        console.log(defaults.a);
+	    };
+
+	    //expose API
+	    return {
+	        init: init
+	    };
+
+	   
+	})();
+
+
+
+
+	module.exports = PanelGallerySPA;
+
 
 /***/ }
 /******/ ])
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hi there!');
+var PanelGallerySPA = (function(){
+
+    console.log('yo!');
+
+    let defaults = {
+        a: 1
+    };
+ 
+    var init = function(){
+        console.log(defaults.a);
+    };
+
+    //expose API
+    return {
+        init: init
+    };
+
+   
+})();
+
+
+
+
+module.exports = PanelGallerySPA;
